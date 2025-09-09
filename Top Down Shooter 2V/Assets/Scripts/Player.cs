1707751e10d6c19a4817b9a85954ba609aa07860
Saulo Daniel Ferreira Pontes <saulodfp@gmail.com>
2025-09-09 14:35:55 -0300

    Adicionado novo projeto Top Down Shooter

---
+++ Top Down Shooter 2V/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Player : Personagem
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            gameObject.transform.position += new Vector3(0, getVelocidade()* Time.deltaTime, 0);
+        }
+        
+        if (Input.GetKey(KeyCode.S))
+        {
+            gameObject.transform.position -= new Vector3(0, getVelocidade()* Time.deltaTime, 0);
+        }
+        
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += new Vector3(getVelocidade()* Time.deltaTime, 0, 0);
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position -= new Vector3(getVelocidade()* Time.deltaTime, 0, 0);
+        }
+    }
+}
