53ee2e69366db7cd4c28645eed8597b711047d44
Saulo Daniel Ferreira Pontes <saulodfp@gmail.com>
2025-09-29 16:20:17 -0300

    inimigos e barra de vida do player

--- Top Down Shooter 2V/Assets/Scripts/Inimigo.cs
+++ Top Down Shooter 2V/Assets/Scripts/Inimigo.cs
@@ -3,11 +3,16 @@ using UnityEngine;
 public class Inimigo : Personagem
 {
     [SerializeField] private int dano = 1;
+    
+    public float raioDeVisao = 1;
+    public CircleCollider2D _visaoCollider2D;
 
     [SerializeField] private Transform posicaoDoPlayer;
     
     private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
+    private bool andando = false;
     
     public void setDano(int dano)
     {
@@ -21,15 +26,20 @@ public class Inimigo : Personagem
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
         
         if (posicaoDoPlayer == null)
         {
             posicaoDoPlayer =  GameObject.Find("Player").transform;
         }
+        
+        raioDeVisao = _visaoCollider2D.radius;
     }
 
     void Update()
     {
+        andando = false;
+        
         if (posicaoDoPlayer.position.x - transform.position.x > 0)
         {
             spriteRenderer.flipX = false;
@@ -41,13 +51,16 @@ public class Inimigo : Personagem
         }
 
 
-        if (posicaoDoPlayer != null)
+        if (posicaoDoPlayer != null && 
+            Vector3.Distance(posicaoDoPlayer.position, transform.position) <= raioDeVisao )
         {
             Debug.Log("Posição do Pluer"+ posicaoDoPlayer.position);
             
             transform.position = Vector3.MoveTowards(transform.position, 
                 posicaoDoPlayer.transform.position,
                 getVelocidade() * Time.deltaTime);
+            
+            andando = true;
         }
         
         if (getVida() <= 0)
@@ -55,6 +68,8 @@ public class Inimigo : Personagem
             //desativa o objeto do Inimigo
             gameObject.SetActive(false);
         }
+        
+        animator.SetBool("Andando",andando);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
--- Top Down Shooter 2V/Assets/Scripts/Player.cs
+++ Top Down Shooter 2V/Assets/Scripts/Player.cs
@@ -1,18 +1,25 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : Personagem
 {
     private SpriteRenderer spriteRenderer;
-
+    private Animator animator;
+    
+    private bool andando = false;
+    
     public Transform arma;
     
     void Start()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
     
     void Update()
     {
+        andando = false;
+        
         if (arma.rotation.eulerAngles.z > -90 
             && arma.rotation.eulerAngles.z < 90)
         {
@@ -31,30 +38,38 @@ public class Player : Personagem
         if (Input.GetKey(KeyCode.W))
         {
             gameObject.transform.position += new Vector3(0, getVelocidade()* Time.deltaTime, 0);
+            andando = true;
         }
         
         if (Input.GetKey(KeyCode.S))
         {
             gameObject.transform.position -= new Vector3(0, getVelocidade()* Time.deltaTime, 0);
+            andando = true;
         }
         
         if (Input.GetKey(KeyCode.D))
         {
             gameObject.transform.position += new Vector3(getVelocidade()* Time.deltaTime, 0, 0);
+            andando = true;
         }
         
         if (Input.GetKey(KeyCode.A))
         {
             gameObject.transform.position -= new Vector3(getVelocidade()* Time.deltaTime, 0, 0);
+            andando = true;
         }
         
         
         if (getVida() <= 0)
         {
-            //desativa o objeto do Player
-            gameObject.SetActive(false);
-            
             Debug.Log("Jogador Morreu!!!");
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+            //desativa o objeto do Player
+            //gameObject.SetActive(false);
         }
+        
+        animator.SetBool("Andando", andando);
     }
 }
